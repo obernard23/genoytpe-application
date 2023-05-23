68e45277ea7b343d127af092919df200e749f061
bern <88011609+obernard23@users.noreply.github.com>
2023-05-23 22:32:50 +0100

    app

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const morgan = require("morgan");
+const mongoose = require('mongoose');
+const authRoutes  = require('./Routes/authenticate');
+const cookieParser = require('cookie-parser');
+const {requireAuth,checkUser} = require('./middleware/authmidddleware');
 const Dotenv = require('./.env')
 
 
@@ -10,24 +12,24 @@ const app = express()
 
 mongoose.set('strictQuery',true)
 mongoose.connect(Dotenv.dbUrl,{useNewUrlParser: true})
-.then(result=>console.log('connected to db'))
+.then(result=>{app.listen(Dotenv.PORT,()=>{
+    console.log(`connected to ${Dotenv.PORT}`)
+}),console.log('connected to db')})
 .catch(err=>console.log(err))
 
-//register views 
-app.set('view engin','ejs')
 
-//middle ware
-app.use(express.static('public'))
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cookieParser())
+//register view engine 
+app.set('view engine','ejs');
 
-//listen for server 
-app.listen(Dotenv.PORT,()=>{
-    console.log('connected to port ')
-})
+//for middle ware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 //declare public folder
 app.set('public')
 
-//
+//router for routes
+app.use(authRoutes);
+
